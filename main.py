d354a38ba48d9b4425513c08c4ab1791a3bdc470
Frenz86 <frenzy86@gmail.com>
2022-09-19 14:34:36 +0200

    deploy 1.7

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request,Depends,HTTPException
+from fastapi.responses import StreamingResponse
 from fastapi.templating import Jinja2Templates
-from starlette.responses import StreamingResponse
+from fastapi.responses import HTMLResponse
+from starlette.responses import JSONResponse
+import pandas as pd
 import uvicorn
-import os 
+from pydantic import BaseModel
+import joblib
+import os
 
 app = FastAPI(title="API1", description="with FastAPI by Daniele Grotti", version="1.0")
 templates = Jinja2Templates(directory="templates")
+
+classes = {
+    0: 'setosa',
+    1: 'versicolor',
+    2: 'virginica'
+}
+
+class Feature_type(BaseModel):
+    #description: Optional[str] = None questo è un campo opzionale
+    feature1 : float = 3.0
+    feature2 : float = 3.0
+    feature3 : float = 3.0
+    feature4 : float = 3.0
+
+
+model = joblib.load("iris.pkl")
 
 
 @app.get("/")
@@ -22,9 +43,18 @@ def loadplot():
     file = open('dog.jpg', mode="rb")
     return StreamingResponse(file, media_type="image/png")
 
-@app.get("/predict")
-def precit():
-    return {"classe": "iris"} 
+@app.get("/predict", response_class=HTMLResponse)
+async def predict_get(data: Feature_type= Depends()):              # depends() input nelle celle
+    try:
+        data = pd.DataFrame(data)
+        data = data.T
+        data.rename(columns=data.iloc[0], inplace = True)
+        data= data.iloc[1:] #must have array
+        
+        y_pred = list(map(lambda x: classes[x], model.predict(data).tolist()))[0]
+        return JSONResponse(y_pred)
+    except:
+        raise HTTPException(status_code=404, detail="error") 
 
 @app.post("/predict")
 def precit():
